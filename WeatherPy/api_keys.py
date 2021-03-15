97f4219bb5c87b814069b84b50ae608f9bb7f330
Max Nguyen <chadwick.uxd@gmail.com>
2021-03-14 20:37:16 -0400

    push

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "ec33115899d8ed21281c77c430494754"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyD3BR1wjmMMn2VFc6V9_fEd1We8w9j8mB0"
